5da2996a86cf1d54abdd477390072ace6be03bfe
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-02-16 11:34:05 -0300

    app

--- js/script.js
+++ js/script.js
@@ -7,22 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function closeMenu() {
         mobileMenu.classList.add('hidden');
-        timeoutId = null; // Limpa o ID do timeout
+        timeoutId = null;
     }
 
     menuToggle.addEventListener('click', function () {
         mobileMenu.classList.toggle('hidden');
 
-        // Limpa qualquer timeout existente
         if (timeoutId) {
             clearTimeout(timeoutId);
         }
 
-        // Define um timeout para fechar o menu após 3 segundos (ajuste conforme necessário)
         timeoutId = setTimeout(closeMenu, 3000);
     });
 
-    // Fecha o menu se o usuário clicar fora do menu e do botão
     document.addEventListener('click', function (event) {
         const isClickInsideMenu = mobileMenu.contains(event.target);
         const isClickInsideButton = menuToggle.contains(event.target);
@@ -32,14 +29,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Vai diretamente para a seção com rolagem suave
     menuLinks.forEach(link => {
         link.addEventListener('click', function (event) {
-            event.preventDefault(); // Impede o comportamento padrão do link (navegar)
-            const targetId = this.getAttribute('href').substring(1); // Pega o ID do alvo (ex: "menu")
-            const targetSection = document.getElementById(targetId); // Encontra a seção
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
 
-            // Rola suavemente para a seção
             targetSection.scrollIntoView({ behavior: 'smooth' });
 
             if (!mobileMenu.classList.contains('hidden')) {
